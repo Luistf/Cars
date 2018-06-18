416fee8abf9630550ed3cf43fadb63c809dc1a7f
Luistf <luistf.book@gmail.com>
2018-06-18 18:33:34 -0300

    Create ViewControllers

---
+++ Cars/Cars/Classes/About/AboutViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AboutViewController.swift
+//  Cars
+//
+//  Created by Luis Henrique on 18/06/2018.
+//  Copyright © 2018 Luis Henrique. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // title for navigation bar
+        self.title = "About"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
