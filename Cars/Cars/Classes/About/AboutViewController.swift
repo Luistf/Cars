dcbdae6fb5b9f7967ea7507514abc9312366b02f
Luistf <luistf.book@gmail.com>
2018-06-20 21:42:40 -0300

    create tableView and include function viewDidAppear

--- Cars/Cars/Classes/About/AboutViewController.swift
+++ Cars/Cars/Classes/About/AboutViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 
 let URL_ABOUT = "http://www.livroiphone.com.br/carros/sobre.htm"
-class AboutViewController: UIViewController {
+class AboutViewController: UIViewController, UIWebViewDelegate {
 
     @IBOutlet var webView : UIWebView!
     @IBOutlet var progress : UIActivityIndicatorView!
     
-    override func viewDidLoad() {
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         super.viewDidLoad()
         // title for navigation bar
         self.title = "About"
@@ -22,8 +23,16 @@ class AboutViewController: UIViewController {
         self.progress.startAnimating()
         // carries the URL
         let url = URL(string: URL_ABOUT)
-        let request = URLRequest(url:url!)
+        let request = URLRequest(url: url!)
         self.webView.loadRequest(request)
+        // delegate
+        self.webView.delegate = self
+    }
+    
+    // method of protocol
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        // stoped animation
+        progress.stopAnimating()
     }
 
     override func didReceiveMemoryWarning() {
--- Cars/Cars/Classes/CarList/CarListViewController.swift
+++ Cars/Cars/Classes/CarList/CarListViewController.swift
@@ -8,12 +8,31 @@
 
 import UIKit
 
-class CarListViewController: UIViewController {
-
+class CarListViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet var tableView : UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // title for navigation bar
         self.title = "Cars"
+        // delegate
+        self.tableView.dataSource = self
+        // this row is necessary for to use ReusableCellWithIdentifier
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10 // return 10 rows
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // create cell this row
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "cell")!
+        let row = indexPath.row
+        cell.textLabel!.text = "Car \(row)"
+        cell.imageView!.image = UIImage(named: "ferrari_ff.png")
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
